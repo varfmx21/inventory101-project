f310772e5be84b6f454eaa7e1012a09a5dd07481
ElValde <luisvalderrabano9@gmail.com>
2024-11-22 18:39:19 -0600

    Adding task 3

--- main.cpp
+++ main.cpp
@@ -1,13 +1,43 @@
 #include "Inventory.h"
+#include "History.h"
+#include <iostream>
 
 int main() {
 
     Inventory inventory;
+    History history;
 
     string product1 = "audifonos";
+    int choice;
 
-    inventory.addProduct(product1, 3);
-    inventory.getProduct(product1);
+    do {
+        cout<<"Inventario AKIBA COMICS 2024"<<'\n';
+        cout<<"1.- Agregar un producto: "<<'\n';
+        cout<<"5.- Salir del inventario"<<'\n';
+        cout<<"Eliga una opcion: ";
+        cin >> choice;
+
+        string nombre;
+        int cantidad;
+        switch(choice) {
+            case 1: {
+                cout<<"Nombre del producto: ";
+                cin>>nombre;
+                cout<<"Cantidad: ";
+                cin>>cantidad;
+                inventory.addProduct(nombre, cantidad);
+                break;
+            }
+            case 5: {
+                cout<<"Saliendo del Inventario"<<'\n';
+                break;
+            }
+            default: {
+                cout<<"Opcion no valida"<<'\n';
+                break;
+            }
+        }
+    } while(choice != 5);
 
     return 0;
 }
